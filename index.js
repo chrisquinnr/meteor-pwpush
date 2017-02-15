99d5708a081c03ce48234f7a375eda912ed4c20e
Chris Quinn <chris@gravitywell.co.uk>
2017-02-15 21:34:48 +0000

    first pas

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var request = require('request');
 exports.meteorpasswords = function() {
-  console.log("logged from my-package");
+//  console.log('Generating secure password link');
+ // var payload = {password:{payload:'gremlin', expire_after_days:3,expire_after_views:5}};
+ // console.log(payload);
+ // request.post('https://pwppush.com/p.json', payload, function(err,httpResponse,body){ 
+ //   console.log(err);
+ //   console.log(httpResponse);
+ //   console.log(body); 
+ // })
 };
+
